af6d9bb57565ebc5bff712a11958748b54ac6e06
Kavindu Sanjula <ravanadevs@gmail.com>
2024-03-07 14:46:27 +0530

    Add map api

--- src/components/Train.jsx
+++ src/components/Train.jsx
@@ -2,12 +2,27 @@ import { Container } from 'react-bootstrap'
 import '../App.css'
 import NavBar from './Navbar'
 
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+
 const Train = () => {
+  const center = [8.059958, 80.273583]
   return (
     <>
       <NavBar />
       <div className='home-body'>
-        <Container className='d-flex flex-column h-100'>map</Container>
+        <Container className='h-100 d-flex justify-content-center align-items-center'>
+          <MapContainer
+            center={center}
+            zoom={15}
+            style={{ height: '600px', width: '800px', borderRadius: '20px' }}
+          >
+            <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+            <Marker position={center}>
+              <Popup>Your train is hear</Popup>
+            </Marker>
+          </MapContainer>
+        </Container>
       </div>
     </>
   )
